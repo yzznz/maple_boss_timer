5a815eff416443a6dd43459b35c1344db237ddf1
chae_yunjae <chae_yunjae@naver.com>
2022-06-27 00:01:04 +0900

    initialize

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(1800);
+  const [patternTime, setPatternTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer_module">
+        <div className="top">
+          <h1>{currentTime}</h1>
+        </div>
+        <div className="middle">
+          <span className="left">
+            <input
+              type="button"
+              value="<"
+              onClick={() => {
+                setPatternTime(patternTime - 1);
+              }}
+            />
+          </span>
+          <span className="center">
+            <h1>{patternTime}</h1>
+          </span>
+          <span className="right">
+            <input
+              type="button"
+              value=">"
+              onClick={() => {
+                setPatternTime(patternTime + 1);
+              }}
+            />
+          </span>
+        </div>
+        <div className="bottom">
+          <h1>{remainingTime}</h1>
+        </div>
+      </div>
     </div>
   );
 }
